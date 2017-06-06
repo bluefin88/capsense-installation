bbf57d5ffbf178363192e900d33d51b89d745e78
bluefin88 <wallacewong@Analog-Folk-Wallace.local>
2017-06-06 12:14:15 +0800

    Update latest Code
    Can now connect reliably up to 13 sensors.

--- capsense_debounce_arduino_wavTrigger.ino
+++ capsense_debounce_arduino_wavTrigger.ino
@@ -4,39 +4,42 @@
 
 wavTrigger wTrig;                    // Our WAV Trigger object
 
-const int led = 13;
-const int delayTime = 50;
-
-
 // CAPSENSE
-const int resolution = 100;          // Settings
-const int sensitivity = 600;         // Settings
+const int resolution = 30;          // Settings
+const long sensitivity = 250;         // Settings
 const int debounce = 200;            // Settings
 
 // Construct Capsense objects and store in array
+// Pin 6 has low sensitivity
 CapacitiveSensor cs[] = {
+  CapacitiveSensor(23, 2),
+  CapacitiveSensor(23, 3),
   CapacitiveSensor(23, 4),
-  CapacitiveSensor(23, 5),
-  CapacitiveSensor(23, 6),
+  CapacitiveSensor(23, 5), 
   CapacitiveSensor(23, 7),
   CapacitiveSensor(23, 8),
-  CapacitiveSensor(23, 9)
+  CapacitiveSensor(23, 9),
+  CapacitiveSensor(23, 10),
+  CapacitiveSensor(23, 11),
+  CapacitiveSensor(23, 12),
+  CapacitiveSensor(23, 13),
+  CapacitiveSensor(23, 24),
+  CapacitiveSensor(23, 25)
 };
 
-// Delare Time and Sensor Arrays
-const int NUMBER_OF_SENSORS = 6;
+// Delay Time and Sensor Arrays
+const int NUMBER_OF_SENSORS = 13;
 long timeArr[NUMBER_OF_SENSORS];
-long sensorArr[NUMBER_OF_SENSORS];
 
 
 void setup()
 { 
    // Initialize Arduino LED and Serial
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
+  //pinMode(led, OUTPUT);
 
   // Initialize Capsense Array Objects
-  for (int i = 0; i++; i < NUMBER_OF_SENSORS - 1) {
+  for (int i = 0; i++; i < NUMBER_OF_SENSORS) {
     timeArr[i] = 0;                                 // Set all sensor timers to 0...
     cs[i].set_CS_AutocaL_Millis(0xFFFFFFFF);        // Calibrate Capsense sensors...
   }
@@ -45,36 +48,21 @@ void setup()
   wTrig.start();
   delay(1000);
 }
-void loop(){
-  
-  // Read sensors and store it in sensorObj Array, required because sensorArr type is long, different from capsense type
-  for(int i=0; i<NUMBER_OF_SENSORS; i++){
-    sensorArr[i] = cs[i].capacitiveSensor(resolution);
-    //for Calibration CHANGED TO 1 
-    Serial.println(sensorArr[3]);
-  }
 
-  // Conditional to check if any of the values in the sensorArr exceed threshold, if true, sensor is active
-  // Sensor activation placed in a separate function in anticipation of other potential behavour when sensor is active, i.e. LED Lights
-  for(int i=0; i<NUMBER_OF_SENSORS; i++){
-    
-    if (sensorArr[i]>sensitivity){
+void loop(){
+  for(int i = 0; i < NUMBER_OF_SENSORS; i++){
+    long reading = cs[i].capacitiveSensor(resolution);
+    if (reading > sensitivity) {
       activateSensor(i);
     }
+    delay(10);
   }
-  delay(delayTime);
 }
 
-
 void activateSensor(int x){
+  //Sends command to WAV trigger to play a track after checking debounce
   if (millis() - timeArr[x] > debounce) {
-    Serial.println(millis() - timeArr[x]);
-    Serial.print("Sensor = ");
-    Serial.println(x);
-    Serial.println(sensorArr[x]);
-    wTrig.trackStop(x+4); // Stop track that is playing already? 
-    wTrig.trackPlayPoly(x+4);   // Play relevant track CHANGED to +4 To play 004,005,006
+    wTrig.trackPlayPoly(x+1);
   }
   timeArr[x] = millis();
 }
-
